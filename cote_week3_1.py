dc0b4d5f6bfa375add450385244ba3e08299b11e
이상원 <sangwon020214@gmail.com>
2026-04-04 23:17:36 +0900

    Solved: 모의 코딩 테스트 3주차

---
+++ cote_week3_1.py
@@ -0,0 +1,14 @@
+m, d = map(int, input().split())
+
+if m == 1 and d == 7:
+    print("Yes")
+elif m == 3 and d == 3:
+    print("Yes")
+elif m == 5 and d == 5:
+    print("Yes")
+elif m == 7 and d == 7:
+    print("Yes")
+elif m == 9 and d == 9:
+    print("Yes")
+else:
+    print("No")
